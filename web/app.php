2176d7f315bc358565ce3682cf780ccb3ce973df
Laurent STELLA <lstella>
2020-02-12 13:30:10 +0000

    removed PHPstorm .idea files commit a forgotten fix for prod

--- web/app.php
+++ web/app.php
@@ -3,7 +3,6 @@
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../app/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
