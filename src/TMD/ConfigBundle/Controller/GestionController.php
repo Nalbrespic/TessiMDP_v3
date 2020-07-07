516ec24b0292affe4c51127c35423e6a99abaffb
Nalbrespic <nadege.albrespic@tessi.fr>
2020-07-07 09:39:19 +0200

    Changement de statut ok

--- src/TMD/ConfigBundle/Controller/GestionController.php
+++ src/TMD/ConfigBundle/Controller/GestionController.php
@@ -87,111 +87,11 @@ class GestionController extends Controller
         ));
     }
 
-    public function gestioncmdAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $clients = $em->getRepository('TMDProdBundle:Client')->findBy(array(),
-            array('nomclient' => 'ASC')
-        );
-
-        return $this->render('TMDConfigBundle:gestion:gestionCmde.html.twig', array(
-            'clients' => $clients
-        ));
-    }
-
-    public function gestioncmdClientAction(Request $request)
-    {
-
-        $idClient = $request->get('idClient');
-        dump($idClient);
-        $em = $this->getDoctrine()->getManager();
-        $clients = $em->getRepository('TMDProdBundle:Client')->findBy(array(),
-            array('nomclient' => 'ASC')
-        );
-        $client = $em->getRepository('TMDProdBundle:Client')->findBy(
-            array('idclient' => $idClient
-            ));
-//        $listtrackings = $em->getRepository('TMDProdBundle:EcommTracking')->trackingByIdclient($idClient);
-//        $trackings = $this->get('knp_paginator')->paginate($listtrackings,
-//            $request->query->get('page', 1), 25);
-        $listOp = $em->getRepository('TMDProdBundle:EcommAppli')->findBy(
-            array('idclient'=>$idClient)
-        );
-
-        dump($listOp);
-
-        return $this->render('TMDConfigBundle:gestion:gestionCmde.html.twig', array(
-            'clients' => $clients,
-            'client' => $client[0],
-            'idClient' => $idClient,
-            'listOpe' => $listOp
-        ));
-    }
-
-    /**
-     * @param Request $request
-     */
-    public function gestionClientOpeAction(Request $request)
-    {
-        $idClient = $request->get('idClient');
-        $idappli = $request->get('idappli');
-        dump($idClient);
-        dump($idappli);
-    }
-    /**
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function editTrackingAction( Request $request)
-    {
-        $numligne = $request->get('numligne');
-        dump($numligne);
-        $em = $this->getDoctrine()->getManager();
-        $tracking = $em->getRepository('TMDProdBundle:EcommTracking')->trackingByNumligne($numligne);
-        $tracking = $tracking['0']['0'];
-
-       $form = $this->createFormBuilder($tracking)
-
-           ->add('refClient', TextType::class, [
-               'attr' => array('readonly' => true ),
-               'label' => 'Mdofier le statut de la commande'
-           ])
-           ->add('idStatut',EntityType::class, [
-               'class' => EcommStatut::class,
-               'choice_label' => 'statut'
-           ])
-           ->add('save', SubmitType::class, [
-               'label'=> 'Modifier le statut'
-           ])
-           ->getForm();
-
-        dump($request);
-        dump($tracking);
-        dump($form);
-        $form->handleRequest($request);
-        $idClient = $tracking->getIdclient()->getIdclient();
-
-        if ($request->isMethod('POST')){
 
 
-            if ($form->isValid() && $form->isSubmitted()){
-
-                dump('modif ok');
-                $em->flush();
 
 
-                return $this->redirectToRoute('tmd_config_gestioncmdClient',[
-                    'idClient'=> $idClient
-                ]);
-            }
 
-        }
-
-        return $this->render('TMDConfigBundle:gestion:editTracking.html.twig', array(
-            'tracking' => $tracking,
-            'form' => $form->createView()
-        ));
-    }
 
 
 }
